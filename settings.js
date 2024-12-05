775eb603865bf2b1b5189b9efea5e42f533fb5e0
Vajira Rathnayaka <158836060+VajiraTech@users.noreply.github.com>
2024-12-05 14:50:43 +0530

    🔱 ＶＡＪＩＲＡ - ＭＤ - Ｖ7 🔱

---
+++ settings.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+
+module.exports = {
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Put your session id here' : process.env.SESSION_ID,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,
+N_JID: process.env.N_JID=== undefined ? '‌': process.env.N_JID,    
+PREFIX: process.env.PREFIX || '.' ,
+FOOTER: process.env.FOOTER=== undefined ? '‌👨‍💻 ᴠᴀᴊɪʀᴀ ᴍᴅ ʙʏ ᴛᴄ ᴛᴇᴀᴍ 👨‍💻': process.env.FOOTER,
+POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,   
+MAX_SIZE: 500,
+PAIR: 'https://stuck-godiva-vajiratech2003-a4248d0c.koyeb.app/code?number=',       
+ALIVE:  process.env.ALIVE  || '> VAJIRA MD'  ,    
+DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO        
+};
