16aa69841c5c52ab44d62d0d7648f60ab2bb19b3
Vajira Rathnayaka <158836060+VajiraTech@users.noreply.github.com>
2024-12-06 21:39:31 +0530

    🔱 ＶＡＪＩＲＡ - ＭＤ - Ｖ7 🔱

--- settings.js
+++ settings.js
@@ -13,7 +13,7 @@ PREFIX: process.env.PREFIX || '.' ,
 FOOTER: process.env.FOOTER=== undefined ? '‌👨‍💻 ᴠᴀᴊɪʀᴀ ᴍᴅ ʙʏ ᴛᴄ ᴛᴇᴀᴍ 👨‍💻': process.env.FOOTER,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,   
 MAX_SIZE: 500,
-PAIR: 'https://stuck-godiva-vajiratech2003-a4248d0c.koyeb.app/code?number=',       
+PAIR: 'https://vajiraqrveb-1d064bcd8502.herokuapp.com/code?number=',       
 ALIVE:  process.env.ALIVE  || '> VAJIRA MD'  ,    
 DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO        
 };
